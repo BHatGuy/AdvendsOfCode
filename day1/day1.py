7ba9ce7f4c8b786a3a1e4dde1fc0ec22dd82c4df
Malte Müller <malte@malte-mueller.eu>
2020-12-01 11:30:58 +0100

    Day 1 with BJ&H

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
+import itertools
+from operator import mul
+from functools import reduce
+
 
 def read_input():
     res = []
-    with open("day1_2.inp", "r") as f:
+    with open("day1.inp", "r") as f:
         for line in f.readlines():
             res.append(int(line))
     return res
 
 
+def special_sum(x):
+    return (sum(x), x)
+
+
 numbers = read_input()
 
-for i in range(0, len(numbers)):
-    for j in range(0, len(numbers)):
-        for k in range(0, len(numbers)):
-            if numbers[i] + numbers[j] + numbers[k] == 2020:
-                print(numbers[i] * numbers[j] * numbers[k])
+# Change length for the respective task
+combis = itertools.combinations(numbers, 3)
+
+sums = map(special_sum, combis)
+
+res = filter((lambda a: a[0] == 2020), sums)
+
+triple = list(res)[0][1]
+prod = reduce(mul, triple)
+print(prod)
