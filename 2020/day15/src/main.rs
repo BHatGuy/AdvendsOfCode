621586c69b822ab7c585f4ccc3880003d1d76b8e
Malte Müller <malte@malte-mueller.eu>
2020-12-16 10:36:27 +0100

    Day 15

--- 2020/day15/src/main.rs
+++ 2020/day15/src/main.rs
@@ -1,3 +1,34 @@
+use std::collections::HashMap;
+static INPUT: &[u32] = &[14, 1, 17, 0, 3, 20];
+static INPUT_TEST: &[u32] = &[0, 3, 6];
+
+fn solve(n: usize) -> u32 {
+    let mut numbers = Vec::from(INPUT);
+    let mut last_positions = HashMap::new();
+    let mut turn = 0;
+    for i in 0..(numbers.len() - 1) {
+        last_positions.insert(numbers[i], turn);
+        turn += 1;
+    }
+    turn += 1;
+    for _ in 0..(n - numbers.len()) {
+        let last = *numbers.last().unwrap();
+
+        let pos = last_positions.get(&last);
+        let next = match pos {
+            Some(x) => numbers.len() - x - 1,
+            None => 0,
+        };
+        numbers.push(next as u32);
+
+        last_positions.insert(last, turn - 1);
+
+        turn += 1;
+    }
+    return *numbers.last().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Solution 1: {}", solve(2020));
+    println!("Solution 2: {}", solve(30000000));
 }
