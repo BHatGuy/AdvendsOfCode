57ac3e76791d551c1e7b406d707c35095acaa78e
Malte Müller <malte@malte-mueller.eu>
2021-12-16 17:19:04 +0100

    Day 16 2

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -21,15 +21,15 @@ fn get_input(name: &str) -> Vec<InputType> {
     input
 }
 
-fn print_bits(bits: &[bool]) {
+fn _print_bits(bits: &[bool]) {
     for &b in bits {
-        print!("{}", if b {1} else {0});
+        print!("{}", if b { 1 } else { 0 });
     }
     println!();
 }
 
-fn as_u16(bits: &[bool]) -> u16 {
-    if bits.len() > 16 {
+fn as_int(bits: &[bool]) -> u64 {
+    if bits.len() > 64 {
         panic!("too long: {:?}", bits);
     }
     let mut byte = 0;
@@ -41,73 +41,94 @@ fn as_u16(bits: &[bool]) -> u16 {
     byte
 }
 
-fn parse_literal(bits: &[bool]) -> usize {
+fn parse_literal(bits: &[bool]) -> (u64, usize) {
     let mut sum = 0;
     let mut length = 0;
     for group in bits.chunks(5) {
         length += 5;
-        sum = (sum << 4) + as_u16(&group[1..]);
+        sum = (sum << 4) + as_int(&group[1..]);
         if !group[0] {
             break;
         }
     }
-    length
+    (sum.into(), length)
 }
 
-fn parse_operator(bits: &[bool], versions: &mut Vec<u16>) -> usize {
+fn parse_operator(bits: &[bool], op: u64, versions: &mut Vec<u64>) -> (u64, usize) {
     let length_id = bits[0];
     let bits = &bits[1..];
+    let mut vals = Vec::new();
+    let mut length;
     if !length_id {
-        let length = as_u16(&bits[0..15]) as usize;
+        length = as_int(&bits[0..15]) as usize;
         let mut rest = &bits[15..];
         let mut len = length;
         while len > 0 {
-            let p = parse_packet(rest, versions);
-            len -= p;
-            rest = &rest[p..];
+            let (val, p_len) = parse_packet(rest, versions);
+            vals.push(val);
+            len -= p_len;
+            rest = &rest[p_len..];
         }
-        return length + 15 + 1;
+        length = length + 15 + 1;
     } else {
-        let mut length = 0;
-        let count = as_u16(&bits[0..11]);
+        length = 0;
+        let count = as_int(&bits[0..11]);
         let mut rest = &bits[11..];
         for _ in 0..count {
-            let p = parse_packet(rest, versions);
-            length += p;
-            rest = &rest[p..];
+            let (val, p_len) = parse_packet(rest, versions);
+            vals.push(val);
+            length += p_len;
+            rest = &rest[p_len..];
         }
-        return length + 11 + 1;
+        length = length + 11 + 1;
     }
-}
-
-fn parse_packet(bits: &[bool], versions: &mut Vec<u16>) -> usize {
-    print_bits(bits);
-    let version = &bits[0..3];
-    versions.push(as_u16(version));
-    let typ = &bits[3..6];
-    let rest = &bits[6..];
-    let length = match as_u16(typ) {
-        4 => parse_literal(rest),
-        _ => parse_operator(rest, versions),
+    let res = match op {
+        0 => vals.iter().sum(),
+        1 => vals.iter().product(),
+        2 => *vals.iter().min().unwrap(),
+        3 => *vals.iter().max().unwrap(),
+        5 => if vals[0] > vals[1] {1} else {0},
+        6 => if vals[0] < vals[1] {1} else {0},
+        7 => if vals[0] == vals[1] {1} else {0},
+        x => panic!("Unknown opcode {}", x), 
     };
 
-    length + 3 + 3
+    (res, length)
 }
 
-fn solve1(inp: &InputType) -> u16 {
+fn parse_packet(bits: &[bool], versions: &mut Vec<u64>) -> (u64, usize) {
+    let version = &bits[0..3];
+    versions.push(as_int(version));
+    let typ = &bits[3..6];
+    let rest = &bits[6..];
+    
+    let op = as_int(typ);
+    let (val, length) = if op == 4 {
+        parse_literal(rest)
+    } else {
+        parse_operator(rest, op, versions)
+    };
+
+    (val, length + 3 + 3)
+}
+
+fn solve1(inp: &InputType) -> u64 {
     let mut versions = Vec::new();
     parse_packet(inp, &mut versions);
     versions.iter().sum()
 }
 
-// fn solve2(_: &InputType) -> u64 {
-//     0
-// }
+fn solve2(inp: &InputType) -> u64 {
+    let mut versions = Vec::new();
+    let res = parse_packet(inp, &mut versions);
+    res.0
+}
 
 fn main() {
     let input = get_input("input.txt");
     for inp in input.iter() {
         println!("1: {}", solve1(&inp));
-        // println!("2: {}", solve2(&inp));
+        println!("2: {}", solve2(&inp));
+        println!();
     }
 }
