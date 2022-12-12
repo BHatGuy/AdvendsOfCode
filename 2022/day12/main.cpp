e83621fd5e76ec80f3ab2053c7c41a04b7ffd48a
Malte Müller <malte@malte-mueller.eu>
2022-12-12 19:28:23 +0100

    Day 12 finished

--- 2022/day12/main.cpp
+++ 2022/day12/main.cpp
@@ -19,51 +19,20 @@ struct Node {
 
     char elevation;
     int distance = -1;
-    int estimate = -1;
     Node* predecessor = nullptr;
 };
 
-void solve(string filename) {
-    std::ifstream input(filename);
-    vector<vector<Node*>> map;
-
-    string line;
-
-    Node* start;
-    Node* end;
-    int x = 0, y = 0;
-    while (std::getline(input, line)) {
-        vector<Node*> row;
-        for (char& c : line) {
-            row.push_back(new Node{x, y, c});
-            Node* node = row.back();
-
-            if (c == 'S') {
-                start = node;
-                start->elevation = 'a';
-                start->distance = 0;
-                start->estimate = 0;
-            }
-            if (c == 'E') {
-                end = node;
-                end->elevation = 'z';
-            }
-            x++;
-        }
-        map.push_back(row);
-        x = 0;
-        y++;
-    }
-
+int find_path(vector<vector<Node*>> map, Node* start, Node* end) {
     vector<Node*> visited;
     vector<Node*> pending;
+    start->distance = 0;
     pending.push_back(start);
 
     while (!pending.empty()) {
         Node* current = pending.back();
         pending.pop_back();
         if (current == end) {
-            cout << current->distance << endl;
+            return current->distance;
             break;
         }
         visited.push_back(current);
@@ -89,28 +58,73 @@ void solve(string filename) {
                     continue;
                 successor->predecessor = current;
                 successor->distance = distance;
-                int estimate = distance + std::abs((int)x - end->x) + std::abs((int)y - end->y);
                 if (std::find(pending.begin(), pending.end(), successor) == pending.end()) {
                     pending.push_back(successor);
                 }
             }
         }
-        std::sort(pending.begin(), pending.end(), [](Node* a, Node* b) { return a->estimate > b->estimate; });
+        std::sort(pending.begin(), pending.end(), [](Node* a, Node* b) { return a->distance > b->distance; });
     }
-    Node* n = end;
-    while (n) {
-        n->elevation += 'A' - 'a';
-        n = n->predecessor;
+    return 10000;
+}
+
+void solve(string filename) {
+    std::ifstream input(filename);
+    vector<vector<Node*>> map;
+
+    string line;
+
+    Node* start;
+    Node* end;
+    int x = 0, y = 0;
+    while (std::getline(input, line)) {
+        vector<Node*> row;
+        for (char& c : line) {
+            row.push_back(new Node{x, y, c});
+            Node* node = row.back();
+
+            if (c == 'S') {
+                start = node;
+                start->elevation = 'a';
+            }
+            if (c == 'E') {
+                end = node;
+                end->elevation = 'z';
+            }
+            x++;
+        }
+        map.push_back(row);
+        x = 0;
+        y++;
     }
 
+    cout << find_path(map, start, end) << endl;
+
+    int min = 1000000;
     for (auto row : map) {
         for (auto p : row) {
-            if (p->elevation < 'a') cout << "\x1b[41m";
-            cout << p->elevation;
-            if (p->elevation < 'a') cout << "\x1b[0m";
+            if (p->elevation == 'a') {
+                int d = find_path(map, p, end);
+                min = std::min(d, min);
+            }
         }
-        cout << endl;
     }
+    cout << min << endl;
+
+    // Node* n = end;
+    // while (n) {
+    //     n->elevation += 'A' - 'a';
+    //     n = n->predecessor;
+    // }
+
+    // for (auto row : map) {
+    //     for (auto p : row) {
+    //         if (p->elevation < 'a') cout << "\x1b[41m";
+    //         cout << p->elevation;
+    //         if (p->elevation < 'a') cout << "\x1b[0m";
+    //     }
+    //     cout << endl;
+    // }
 
     for (auto row : map) {
         for (auto p : row) {
